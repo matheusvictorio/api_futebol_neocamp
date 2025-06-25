46ef59e299b7d684008bce46584a4e9a0091e92c
matheusvictorio <matheusvic2016@gmail.com>
2025-06-25 08:29:22 -0300

    Inicial commit: Project Setup

---
+++ src/main/java/com/neocamp/api_futebol/ApiDePartidasDeFutebolApplication.java
@@ -0,0 +1,13 @@
+package com.neocamp.api_futebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDePartidasDeFutebolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDePartidasDeFutebolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neocamp/api_futebol/ApiDePartidasDeFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neocamp.api_futebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDePartidasDeFutebolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
